e7283bc09e857d2d8d794c9bd681b047f2eb79d3
Tom Wilson <tom@jackhq.com>
2016-11-22 14:33:55 -0500

    step 1 - app - home - about

---
+++ web/src/pages/about.js
@@ -0,0 +1,14 @@
+const React = require('react')
+
+const About = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>About Relief Tracker</h1>
+        <p>Version 1033</p>
+      </div>
+    );
+  }
+})
+
+module.exports = About
---
+++ web/src/pages/home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = Home
